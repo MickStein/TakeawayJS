0d4126d79d508029859b531a0979aa1a9f35a38f
michael steinacher <steinacherm@hotmail.co.uk>
2021-11-19 11:10:59 +0000

    first commit

---
+++ src/send_sms.js
@@ -0,0 +1,14 @@
+// Download the helper library from https://www.twilio.com/docs/node/install
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(AC7506b34654059c909fc4c34703074ed8, 08570ebd0577ed8101f59ad3cbb8c0a1);
+
+client.messages
+  .create({
+     body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
+     from: '+15017122661',
+     to: '+15558675310'
+   })
+  .then(message => console.log(message.sid));
